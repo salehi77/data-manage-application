dbaefac11621e7209fdfe03447c48850dd7009c9
Mohammad Salehi <msalehy77@gmail.com>
2019-12-05 09:01:45 +0330

    add tab nav

--- src/Routes.js
+++ src/Routes.js
@@ -1,30 +1,31 @@
+import React, {Component} from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
+import {Text, View, Button} from 'react-native';
 
 import HomeScreen from './components/Screens/Home';
-import ClinicScreen from './components/Screens/Clinic';
 import ClinicMenuScreen from './components/Screens/ClinicMenu';
 import AlgorithmScreen from './components/Screens/Algorithm';
 import DescriptionScreen from './components/Screens/Description';
+import SearchScreen from './components/Screens/Search';
 
-const HomeStack = createStackNavigator(
-  // const HomeStack = createBottomTabNavigator(
-  {
-    Home: HomeScreen,
-    Clinic: ClinicScreen,
-    ClinicMenu: ClinicMenuScreen,
-    Algorithm: AlgorithmScreen,
-    Description: DescriptionScreen,
-  },
-  {
-    initialRouteName: 'Clinic',
-    // defaultNavigationOptions: {
-    //   headerTintColor: '#fff',
-    //   headerStyle: {
-    //     backgroundColor: '#689',
-    //   },
-    // },
-  },
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  ClinicMenu: ClinicMenuScreen,
+  Algorithm: AlgorithmScreen,
+  Description: DescriptionScreen,
+});
+
+const SearchStack = createStackNavigator({
+  Search: SearchScreen,
+});
+
+const TabNavigator = createAppContainer(
+  createBottomTabNavigator({
+    Home: HomeStack,
+    Search: SearchStack,
+  }),
 );
 
-export default HomeStack;
+export default TabNavigator;
--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,18 +1,111 @@
-import React from 'react';
-import {Text, View, Button} from 'react-native';
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import {connect} from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-class HomeScreen extends React.Component {
+class Clinic extends Component {
+  static navigationOptions = {
+    title: 'کلینیک‌ها',
+  };
+
+  state = {
+    data: null,
+  };
+
+  componentDidMount() {
+    const {sqlite} = this.props;
+
+    sqlite.transaction(tx => {
+      tx.executeSql('SELECT * FROM clinic')
+        .then(res => {
+          this.setState({
+            data: [...res[1].rows.raw()],
+          });
+        })
+        .catch(error => {
+          console.error('err');
+        });
+    });
+  }
+
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Home!</Text>
-        <Button
-          title="go"
-          onPress={() => this.props.navigation.navigate('Clinic')}
-        />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: this.props.theme.PRIMARY_BACKGROUND_COLOR,
+        }}>
+        {this.state.data ? (
+          <View style={styles.clinicsWrapper}>
+            {this.state.data.map((clinic, i) => {
+              return (
+                <TouchableOpacity
+                  key={clinic.ID}
+                  style={styles.clinicButton}
+                  onPress={() => {
+                    this.props.navigation.navigate('ClinicMenu', {
+                      clinicID: clinic.ID,
+                      clinicName: clinic.name,
+                    });
+                  }}>
+                  <Icon
+                    name={clinic.icon_name}
+                    size={50}
+                    color={clinic.icon_color}
+                  />
+                  <Text
+                    style={{
+                      fontFamily: this.props.theme.PRIMARY_FONT_FAMILY_BOLD,
+                      color: this.props.theme.PRIMARY_TEXT_COLOR,
+                      fontSize: this.props.theme.FONT_SIZE_MEDIUM,
+                    }}>
+                    {clinic.name}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        ) : (
+          <Text>isloaing...</Text>
+        )}
       </View>
     );
   }
 }
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+  clinicsWrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    marginTop: '3%',
+  },
+  clinicButton: {
+    marginHorizontal: 5,
+    marginVertical: 10,
+    width: '30%',
+    alignItems: 'center',
+  },
+  clinicText: {},
+});
+
+const mapStateToProps = state => {
+  return {
+    sqlite: state.localdb.sqlite,
+    theme: state.theme,
+  };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Clinic);
